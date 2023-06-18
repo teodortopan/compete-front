92848233ca0da7d3ab91e8ab215cd693e3d6ec57
TeoElRey <teodortopancs@gmail.com>
2023-06-18 12:21:05 -0400

    updated backend

--- back-end/server/server.js
+++ back-end/server/server.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import { Pool } from 'pg'
+const express = require('express')
+const { Pool } = require('pg')
 // Load privacy-sensitive information from .env file
 require('dotenv').config()
 const dbUser = process.env.PSQL_USER
@@ -17,11 +17,11 @@ const pool = new Pool({
 })
 
 const app = express()
-// Define GET request to pull data from postgreSQL table
-app.get('/users', async (req, res) => {
+// Define GET request to pull user account data from postgreSQL table
+app.get('/user_accounts', async (req, res) => {
   try {
     // Select all rows from the users table
-    const result = await pool.query('SELECT * FROM users');
+    const result = await pool.query('SELECT * FROM user_accounts');
     res.json(result.rows);
   } catch (error) {
     console.error('Error executing query', error);
@@ -29,12 +29,38 @@ app.get('/users', async (req, res) => {
   }
 })
 
-// Define POST request to post data to postgreSQL table
-app.post('/users', async (req, res) => {
+// Define POST request to post user account data to postgreSQL table
+app.post('/user_accounts', async (req, res) => {
   try {
     const { name, email, password } = req.body;
     // Insert data into the users table
-    const query = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)';
+    const query = 'INSERT INTO user_accounts (username, email, password) VALUES ($1, $2, $3)';
+    await pool.query(query, [name, email, password]);
+
+    res.sendStatus(200) // Send a success response
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Server error' });
+  }
+})
+
+app.get('/competitions', async (req, res) => {
+  try {
+    // Select all rows from the users table
+    const result = await pool.query('SELECT * FROM competitions');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+// Define POST request to post user account data to postgreSQL table
+app.post('/competitions', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // Insert data into the users table
+    const query = 'INSERT INTO competitions (username, email, password) VALUES ($1, $2, $3)';
     await pool.query(query, [name, email, password]);
 
     res.sendStatus(200) // Send a success response
